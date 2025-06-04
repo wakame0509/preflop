67e9314affb2637765340aac052f4570e8b373dc
wakame0509 <wakamegek1699@yahoo.ne.jp>
2025-06-04 17:50:35 +0900

    Update generate_static_preflop_winrates.py

--- generate_static_preflop_winrates.py
+++ generate_static_preflop_winrates.py
@@ -1,31 +1,60 @@
 import eval7
 import random
-from hand_range_matrix import PREDEFINED_HAND_RANGES
 
-def estimate_winrate(hero_hand, villain_range, iters=100000):
+# 高速な25%レンジスート付きコンボリスト
+def get_fast_25_percent_combos():
+    suits = ['c', 'd', 'h', 's']
+    top_25_percent_hands = [
+        "AA", "KK", "QQ", "JJ", "TT", "99", "88", "77",
+        "AKs", "AQs", "AJs", "ATs", "KQs", "KJs", "QJs", "JTs",
+        "AKo", "AQo", "AJo", "KQo", "QJo", "T9s", "98s", "87s",
+        "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s"
+    ]
+
+    def generate_combo_list(hand_str):
+        r1, r2 = hand_str[0], hand_str[1]
+        combo_list = []
+        if len(hand_str) == 2:
+            for i in range(len(suits)):
+                for j in range(i + 1, len(suits)):
+                    combo_list.append(r1 + suits[i] + r2 + suits[j])
+        elif hand_str.endswith('s'):
+            for s in suits:
+                combo_list.append(r1 + s + r2 + s)
+        elif hand_str.endswith('o'):
+            for s1 in suits:
+                for s2 in suits:
+                    if s1 != s2:
+                        combo_list.append(r1 + s1 + r2 + s2)
+        return combo_list
+
+    all_combos = []
+    for hand in top_25_percent_hands:
+        all_combos.extend(generate_combo_list(hand))
+    return sorted(set(all_combos))
+
+# 勝率推定
+def estimate_winrate(hero_hand, villain_combos, iters=100000):
     hero = [eval7.Card(card) for card in hero_hand]
     hero_score = 0
-
     for _ in range(iters):
         deck = eval7.Deck()
         for card in hero:
             deck.cards.remove(card)
 
         while True:
-            villain = random.sample(deck.cards, 2)
-            v1, v2 = str(villain[0]), str(villain[1])
-            if v1 + v2 in villain_range or v2 + v1 in villain_range:
+            combo = random.choice(villain_combos)
+            v1, v2 = combo[:2], combo[2:]
+            if v1 in str(deck) and v2 in str(deck):
                 break
 
+        villain = [eval7.Card(v1), eval7.Card(v2)]
         for card in villain:
             deck.cards.remove(card)
 
         board = deck.sample(5)
-        hero_combined = hero + board
-        villain_combined = villain + board
-
-        hero_value = eval7.evaluate(hero_combined)
-        villain_value = eval7.evaluate(villain_combined)
+        hero_value = eval7.evaluate(hero + board)
+        villain_value = eval7.evaluate(villain + board)
 
         if hero_value > villain_value:
             hero_score += 1
@@ -34,6 +63,7 @@ def estimate_winrate(hero_hand, villain_range, iters=100000):
 
     return hero_score / iters
 
+# 全スターティングハンド
 def get_all_starting_hands():
     ranks = "AKQJT98765432"
     hands = []
@@ -47,8 +77,9 @@ def get_all_starting_hands():
                 hands.append(r1 + r2)
     return hands
 
-def estimate_winrate_for_hand_vs_range(hand_str, villain_range, iters=100000):
-    rank1, rank2 = hand_str[0], hand_str[1]
+# 各ハンドで勝率を計算
+def estimate_winrate_for_hand_vs_range(hand_str, villain_combos, iters=100000):
+    r1, r2 = hand_str[0], hand_str[1]
     suited = hand_str.endswith("s")
     offsuit = hand_str.endswith("o")
     suits = ['c', 'd', 'h', 's']
@@ -56,35 +87,36 @@ def estimate_winrate_for_hand_vs_range(hand_str, villain_range, iters=100000):
 
     if suited:
         for s in suits:
-            combos.append([rank1 + s, rank2 + s])
+            combos.append([r1 + s, r2 + s])
     elif offsuit:
         for s1 in suits:
             for s2 in suits:
                 if s1 != s2:
-                    combos.append([rank1 + s1, rank2 + s2])
+                    combos.append([r1 + s1, r2 + s2])
     else:
         for i in range(len(suits)):
             for j in range(i + 1, len(suits)):
-                combos.append([rank1 + suits[i], rank2 + suits[j]])
+                combos.append([r1 + suits[i], r2 + suits[j]])
 
     results = []
     for combo in combos:
         try:
-            wr = estimate_winrate(combo, villain_range, iters // len(combos))
+            wr = estimate_winrate(combo, villain_combos, iters // len(combos))
             results.append(wr)
         except:
             continue
 
     return sum(results) / len(results) if results else 0
 
-def generate_winrates(percent="25%", iters=100000):
+# メイン関数
+def generate_winrates(iters=100000):
     results = {}
-    villain_range = PREDEFINED_HAND_RANGES[percent]
+    villain_combos = get_fast_25_percent_combos()
     all_hands = get_all_starting_hands()
 
     for i, hand in enumerate(all_hands, 1):
         print(f"[{i}/{len(all_hands)}] {hand}")
-        wr = estimate_winrate_for_hand_vs_range(hand, villain_range, iters)
+        wr = estimate_winrate_for_hand_vs_range(hand, villain_combos, iters)
         results[hand] = round(wr * 100, 1)
 
     return results
