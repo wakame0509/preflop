bc98c6d849fb0997a6ee172b55ad0edd7f3fea7e
wakame0509 <wakamegek1699@yahoo.ne.jp>
2025-06-04 17:55:01 +0900

    Update hand_range_matrix.py

--- hand_range_matrix.py
+++ hand_range_matrix.py
@@ -1,18 +1,10 @@
 # hand_range_matrix.py
 
-# ハンドレンジ：25%、30%（PokerStoveなどの標準を参考に手動定義）
 PREDEFINED_HAND_RANGES = {
     "25%": [
         "AA", "KK", "QQ", "JJ", "TT", "99", "88", "77",
-        "AKs", "AQs", "AJs", "ATs", "KQs", "KJs", "QJs", "JTs", "T9s",
-        "AKo", "AQo", "AJo", "KQo", "QJo",
-        "66", "55", "44", "33", "22", "98s", "87s", "76s", "65s"
-    ],
-    "30%": [
-        "AA", "KK", "QQ", "JJ", "TT", "99", "88", "77", "66",
-        "AKs", "AQs", "AJs", "ATs", "A9s", "KQs", "KJs", "KTs",
-        "QJs", "QTs", "JTs", "T9s", "98s",
-        "AKo", "AQo", "AJo", "ATo", "KQo", "KJo", "QJo",
-        "55", "44", "33", "22", "87s", "76s", "65s", "54s"
+        "AKs", "AQs", "AJs", "ATs", "KQs", "KJs", "QJs", "JTs",
+        "T9s", "98s", "87s", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s",
+        "AKo", "AQo", "AJo", "KQo", "QJo"
     ]
 }
